aef2141a255c5cc6337a72c7f75393e93b9b608c
Daniele Bortoluzzi <daniele.bortoluzzi@unito.it>
2024-02-02 10:48:57 +0100

    try to use gateway and source with fixed or radial connection

--- lib/case-study.hpp
+++ lib/case-study.hpp
@@ -38,12 +38,71 @@ namespace tags {
     //! @brief Shape of the current node.
     struct node_shape {};
     // ... add more as needed, here and in the tuple_store<...> option below
+
+    //! @brief The areaLog value reaching the gateway from the anomaly.
+    struct gateway_log {};
 }
 
 //! @brief The maximum communication range between nodes.
 constexpr size_t communication_range = 100;
 
 // [AGGREGATE PROGRAM]
+
+//! @brief Counts the number of messages received by each neighbour.
+FUN field<int> uniConnection(ARGS) { CODE
+    return old(CALL, field<int>{0}, [&](field<int> o){
+        return o + mod_other(CALL, 1, 0);
+    });
+}
+
+//! @brief Counts the number of bidirectional communications with each neighbour.
+FUN field<int> biConnection(ARGS) { CODE
+    return nbr(CALL, field<int>{0}, [&](field<int> n){
+        return n + mod_other(CALL, 1, 0);
+    });
+}
+
+//! @brief Returns the number of hops to the closest device where `source` is true.
+FUN double distanceTo(ARGS, bool source) { CODE
+    return nbr(CALL, INF, [&](field<double> n){
+        return mux(source, 0.0, min_hood(CALL, n, INF)+1.0);
+    });
+}
+
+//! @brief Propagates value along ascending dist.
+GEN(T) T broadcast(ARGS, double dist, T value) { CODE
+    tuple<double,T> loc{dist, value};
+    return get<1>(nbr(CALL, loc, [&](field<tuple<double,T>> n){
+        return make_tuple(dist, get<1>(min_hood(CALL, n, loc)));
+    }));
+}
+
+//! @brief Block C - converge-cast.
+GEN(F, G) double C(ARGS, bool sink, double value, F&& accumulate, G&& divide) { CODE
+    double dist = distanceTo(CALL, sink);
+    return get<1>(nbr(CALL, make_tuple(dist,value), [&](field<tuple<double,double>> n){
+        field<int> conn = mux(get<0>(n) < dist, biConnection(CALL), 0);
+        field<double> send = conn / (double)max(sum_hood(CALL, conn, 0), 1);
+        field<double> recv = nbr(CALL, send);
+        return make_tuple(dist, fold_hood(CALL, accumulate, map_hood(divide, get<1>(n), recv), value));
+    }));
+}
+
+//! @brief Returns the number of hops between the closests source and dest devices.
+FUN int distance(ARGS, bool source, bool dest) { CODE
+    return broadcast(CALL, distanceTo(CALL, source), distanceTo(CALL, dest));
+}
+
+//! @brief Returns true on the way between the closest source and dest devices, false elsewhere
+FUN bool channel(ARGS, bool source, bool dest, double width) { CODE
+    return distanceTo(CALL, source) + distanceTo(CALL, dest) <= distance(CALL, source, dest) + width;
+}
+
+//! @brief Broadcasts the value in source along the channel between source and dest
+GEN(T) T broadcastChannel(ARGS, bool source, bool dest, double width, T value) { CODE
+    return channel(CALL, source, dest, width) ? broadcast(CALL, distanceTo(CALL, source), value) : value;
+}
+
 
 //! @brief Main function.
 MAIN() {
@@ -65,9 +124,37 @@ MAIN() {
     node.storage(node_size{}) = 10;
     node.storage(node_color{}) = color(GREEN);
     node.storage(node_shape{}) = shape::sphere;
+
+    /*****************/
+    bool gateway = node.uid == 0; // gateway is node 0
+    // anomaly is in node 1 between times 25 and 50
+    bool anomaly = node.uid == 1 and 25 < node.current_time() and node.current_time() < 50;
+    double log = 1; // dummy log value (areaLog counts the area size)
+
+    double areaLog = distanceTo(CALL, anomaly) < 10 ? C(CALL, anomaly, log, [](double x, double y){
+        return x+y;
+    }, [](double x, double y){
+        return x*y;
+    }) : 0;
+    double result = broadcastChannel(CALL, anomaly, gateway, 10, areaLog);
+    node.storage(tags::gateway_log{}) = gateway ? result : 0;
+    if (gateway) {
+        node.storage(node_color{}) = color(RED);
+    }
+    if (anomaly) {
+        node.storage(node_color{}) = color(YELLOW);
+    }
+
+    /*****************/
 }
 //! @brief Export types used by the main function (update it when expanding the program).
-FUN_EXPORT main_t = export_list<double, int>;
+FUN_EXPORT main_t = export_list<double, 
+                                int,    
+                                field<int>, 
+                                field<double>, 
+                                tuple<double,double>, 
+                                tuple<field<double>,double>
+                                >;
 
 } // namespace coordination
 
@@ -101,14 +188,16 @@ using rectangle_d = distribution::rect_n<1, 0, 0, 500, 500>;
 using store_t = tuple_store<
     node_color,                 color,
     node_size,                  double,
-    node_shape,                 shape
+    node_shape,                 shape,
+    gateway_log,                double
 >;
 //! @brief The tags and corresponding aggregators to be logged (change as needed).
 using aggregator_t = aggregators<
     node_size,                  aggregator::mean<double>
 >;
 //! @brief Connection predicate (supports power and sleep ratio, 50% loss at 70% of communication range)
-using connect_t = connect::radial<70, connect::powered<coordination::communication_range, 1, dim>>;
+// using connect_t = connect::radial<70, connect::powered<coordination::communication_range, 1, 2>>;
+using connect_t = connect::fixed<100>;
 
 //! @brief The general simulation options.
 DECLARE_OPTIONS(list,
